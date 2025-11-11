cfd20ababd2443ad5fbf2cd6d87eb429cf413139
simatupangrafael100-alt <simatupangrafael100@gmail.com>
2025-11-11 20:57:09 +0700

    Rafael Simatupang
    Selesai

---
+++ TFUNC.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.Math;
+
+public class TFUNC {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n, hasil;
+
+        n = Integer.parseInt(input.nextLine());
+        System.out.println("Bilangan yang dimasukkan (N) : " + n);
+        System.out.println("Hasil perhitungan rekursif: " + "(" + n + ")" + " : " + angka(n));
+    }
+    
+    public static int angka(int n) {
+        int result;
+
+        if (n == 0) {
+            result = 0;
+        } else {
+            result = n + angka(n - 1);
+        }
+        
+        return result;
+    }
+}
